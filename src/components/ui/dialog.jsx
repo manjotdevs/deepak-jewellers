bd91adc31464cbec8b578c776af9f9378a695817
manjot <mssond459@gmail.com>
2024-08-22 22:34:48 +0530

    dialog is added

---
+++ src/components/ui/dialog.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Dialog = ({ isOpen, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center ">
+      <div className=" text-white rounded-lg shadow-lg relative max-w-md mx-auto">
+        <div className="mt-2 mb-4">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+Dialog.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.node,
+};
+
+Dialog.defaultProps = {
+  title: "Dialog Title",
+  children: null,
+};
+
+export default Dialog;
