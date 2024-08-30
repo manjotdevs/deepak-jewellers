ef71bb88110af84387d4a2114c66ada22e135681
manjor <manjot@manjors-Mac-mini.local>
2024-08-31 00:12:01 +0530

    database and storage is added

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,9 +1,10 @@
-import { Client, Account,ID } from "appwrite";
+import { Client, Account,Storage, Databases } from "appwrite";
 import conf from "./env";
 
 const client = new Client();
 client.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProjectId);
 
 const account = new Account(client);
-
-export {account}
+const storage = new Storage(client);
+const database = new Databases();
+export {account,storage,database}
